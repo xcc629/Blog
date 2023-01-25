a25a7835aaa379fc4b1db09527c0ff5d103dcb64
geunhong Lim <seelime11@gmail.com>
2023-01-25 17:12:14 +0900

    feat: prisma, axios, react-query설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,40 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import Header from "../src/blog_component/_component/Header/Header";
 import Divider from "../src/common_component/Divider";
+import WEHeader from "../src/blog_component/_component/WEHeader";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div
-      style={{
-        display: "flex",
-      }}
-    >
-      <main
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <div
         style={{
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "100%",
         }}
       >
-        <Header />
-        <Divider size={1} color="gray50" />
-        <Component {...pageProps} />
-      </main>
-    </div>
+        <main
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Header />
+          <Divider size={1} color="gray50" />
+          {}
+          <WEHeader />
+          <Divider size={1} color="gray50" />
+
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
